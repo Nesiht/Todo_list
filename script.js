5d7ed969fd0b95121ea9e808d06e100c848dc52d
Mathias Thorström <thisen@gmail.com>
2019-12-22 22:24:43 +0100

    remove items button and css

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ const textField = document.getElementById("text-item");
 // Error list
 const A100 = "ERROR A100: Input field empty";
 
-// add items in array when button is clicked
+// Add items to array when button is clicked
 addItem = () => {
-    itemValue = document.getElementById('text-item').value;
+    let itemValue = document.getElementById('text-item').value;
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+
     if (itemValue !== "") {
+        // Create li tag and add value from input field
         items.push(itemValue);
         console.log(items);
         const markUp = document.createElement("li");
         let textnode = document.createTextNode(itemValue);
         markUp.appendChild(textnode);
         document.getElementById("myList").appendChild(markUp);
+        // Add span with className 'close' and add x
+        markUp.appendChild(span);
+        span.className = "close";
+        span.appendChild(txt);
     }
     else {
         console.log(A100);
@@ -36,7 +44,13 @@ textField.addEventListener('keyup', function(event) {
 
 // List all items in the array
 for (i = 0; i < items.length; ++i) {
-    let markUp = document.createElement("li");
+    const markUp = document.createElement("li");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
     markUp.innerHTML = items[i];
     document.getElementById("myList").appendChild(markUp);
-}
+    // Add span with className 'close' and add x
+    markUp.appendChild(span);
+    span.className = "close";
+    span.appendChild(txt);   
+};
